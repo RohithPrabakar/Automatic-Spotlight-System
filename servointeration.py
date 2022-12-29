606496149365405d7e02d0e19385393505b2f17f
RohithPrabakar <65601026+RohithPrabakar@users.noreply.github.com>
2022-12-29 15:31:19 -0500

    servo rotation code

---
+++ servointeration.py
@@ -0,0 +1,38 @@
+import math
+
+#for right and left movement
+def servo1(light_pos, person_pos):      
+    adjacent = abs(person_pos[0] - light_pos[0])
+    opposite = person_pos[1] - light_pos[1]
+    if(opposite == 0):
+        angle = 90
+    else:
+        angle = 90 + math.degrees(math.atan(opposite/adjacent))
+   
+    return angle
+
+
+#for up and down movement
+def servo2(light_pos, person_pos, light_height):
+    opposite = abs(person_pos[0] - light_pos[0])
+    adjacent = light_height
+    angle = math.degrees(math.atan(opposite/adjacent))
+    return angle
+   
+light_height = 3
+light1_pos = [0,5]
+light2_pos = [20,5]
+person_pos = [3,4]
+
+print("Person position")
+print(person_pos[0],person_pos[1])
+print("--------------------------")
+print("servo 2 angles for light 1")
+print(servo2(light1_pos,person_pos,light_height))
+print("servo 1 angles for light 1")
+print(servo1(light1_pos,person_pos))
+print("servo 2 angles for light 2")
+print(servo2(light2_pos,person_pos,light_height))
+print("servo 1 angles for light 1")
+print(servo1(light1_pos,person_pos))
+
